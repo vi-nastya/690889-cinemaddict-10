b9af1ff29e88d1ab138f2aef203ace19ab9555ea
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-13 10:24:37 -0500

    Adds Sort class

---
+++ src/components/sort.js
@@ -0,0 +1,15 @@
+import { AbstractComponent } from "../utils";
+
+export class Sort extends AbstractComponent {
+  constructor() {
+    super();
+  }
+
+  getTemplate() {
+    return `<ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`;
+  }
+}
