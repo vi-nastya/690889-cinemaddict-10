d57b4056a4a0dfe3aed1a184a239f91fd6787d72
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-14 21:52:37 -0500

    Adds mocks for films and comments

---
+++ src/mocks/comments.js
@@ -0,0 +1,23 @@
+export const comments = [
+  {
+    id: `0`,
+    author: `Ilya O'Reilly`,
+    comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+    date: `2019-05-11T16:12:32.554Z`,
+    emotion: `neutral-face`
+  },
+  {
+    id: `1`,
+    author: `Ilya O'Reilly`,
+    comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+    date: `2019-05-11T16:12:32.554Z`,
+    emotion: `neutral-face`
+  },
+  {
+    id: `2`,
+    author: `Ilya O'Reilly`,
+    comment: `a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.`,
+    date: `2019-05-11T16:12:32.554Z`,
+    emotion: `neutral-face`
+  }
+];
---
+++ src/mocks/films.js
@@ -0,0 +1,114 @@
+export const films = [
+  {
+    id: `0`,
+    comments: [`0`, `1`],
+    filmInfo: {
+      title: `A Little Pony Without The Carpet`,
+      alternativeTitle: `Laziness Who Sold Themselves`,
+      totalRating: 5.3,
+      poster: `images/posters/blue-blazes.jpg`,
+      ageRating: 0,
+      director: `Tom Ford`,
+      writers: [`Takeshi Kitano`],
+      actors: [`Morgan Freeman`],
+      release: {
+        date: 1475924187819,
+        releaseCountry: `Finland`
+      },
+      runtime: 77,
+      genre: [`Comedy`],
+      description: `Oscar-winning film, a war drama about two young people, from the creators of timeless classic \`Nu, Pogodi!\` and \`Alice in Wonderland\`, with the best fight scenes since Bruce Lee.`
+    },
+    userDetails: {
+      personalRating: 9,
+      watchlist: false,
+      alreadyWatched: true,
+      watchingDate: `2019-05-11T16:12:32.554Z`,
+      favorite: false
+    }
+  },
+  {
+    id: `0`,
+    comments: [`2`, `1`],
+    filmInfo: {
+      title: `A Little Pony Without The Carpet`,
+      alternativeTitle: `Laziness Who Sold Themselves`,
+      totalRating: 5.3,
+      poster: `images/posters/blue-blazes.jpg`,
+      ageRating: 0,
+      director: `Tom Ford`,
+      writers: [`Takeshi Kitano`],
+      actors: [`Morgan Freeman`],
+      release: {
+        date: 1475924187819,
+        releaseCountry: `Finland`
+      },
+      runtime: 77,
+      genre: [`Comedy`],
+      description: `Oscar-winning film, a war drama about two young people, from the creators of timeless classic \`Nu, Pogodi!\` and \`Alice in Wonderland\`, with the best fight scenes since Bruce Lee.`
+    },
+    userDetails: {
+      personalRating: 9,
+      watchlist: false,
+      alreadyWatched: true,
+      watchingDate: `2019-05-11T16:12:32.554Z`,
+      favorite: false
+    }
+  },
+  {
+    id: `0`,
+    comments: [`1`, `2`],
+    filmInfo: {
+      title: `A Little Pony Without The Carpet`,
+      alternativeTitle: `Laziness Who Sold Themselves`,
+      totalRating: 5.3,
+      poster: `images/posters/blue-blazes.jpg`,
+      ageRating: 0,
+      director: `Tom Ford`,
+      writers: [`Takeshi Kitano`],
+      actors: [`Morgan Freeman`],
+      release: {
+        date: 1475924187819,
+        releaseCountry: `Finland`
+      },
+      runtime: 77,
+      genre: [`Comedy`],
+      description: `Oscar-winning film, a war drama about two young people, from the creators of timeless classic \`Nu, Pogodi!\` and \`Alice in Wonderland\`, with the best fight scenes since Bruce Lee.`
+    },
+    userDetails: {
+      personalRating: 9,
+      watchlist: false,
+      alreadyWatched: true,
+      watchingDate: `2019-05-11T16:12:32.554Z`,
+      favorite: false
+    }
+  },
+  {
+    id: `0`,
+    comments: [`0`, `2`],
+    filmInfo: {
+      title: `A Little Pony Without The Carpet`,
+      alternativeTitle: `Laziness Who Sold Themselves`,
+      totalRating: 5.3,
+      poster: `images/posters/blue-blazes.jpg`,
+      ageRating: 0,
+      director: `Tom Ford`,
+      writers: [`Takeshi Kitano`],
+      actors: [`Morgan Freeman`],
+      release: {
+        date: 1475924187819,
+        releaseCountry: `Finland`
+      },
+      runtime: 77,
+      genre: [`Comedy`],
+      description: `Oscar-winning film, a war drama about two young people, from the creators of timeless classic \`Nu, Pogodi!\` and \`Alice in Wonderland\`, with the best fight scenes since Bruce Lee.`
+    },
+    userDetails: {
+      personalRating: 9,
+      watchlist: false,
+      alreadyWatched: true,
+      watchingDate: `2019-05-11T16:12:32.554Z`,
+      favorite: false
+    }
+  }
+];
