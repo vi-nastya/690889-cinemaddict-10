5ce264858aaaec85f3c0d41d75a8b99667d0f8e9
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-15 13:47:31 -0500

    Adds empty films list case

--- src/main.js
+++ src/main.js
@@ -43,49 +43,72 @@ if (films.length > 0) {
 // RENDER MAIN SECTION
 render(mainContainer, new Menu().getElement(), Position.BEFOREEND);
 render(mainContainer, new Sort().getElement(), Position.BEFOREEND);
-render(mainContainer, new ContentContainer().getElement(), Position.BEFOREEND);
 
-const filmsList = document.querySelectorAll(`.films-list`)[0];
-const filmsContainer = document.querySelectorAll(`.films-list__container`)[0];
-const topFilmsContainer = document.querySelectorAll(
-  `.films-list__container`
-)[1];
-const commentedFilmsContainer = document.querySelectorAll(
-  `.films-list__container`
-)[2];
+if (films.length === 0) {
+  render(
+    mainContainer,
+    `There are no movies in our database`,
+    Position.BEFOREEND
+  );
+} else {
+  render(
+    mainContainer,
+    new ContentContainer().getElement(),
+    Position.BEFOREEND
+  );
 
-for (let i = 0; i < NUM_FILMS; i++) {
-  const film = new FilmCard(films[i]);
-  const filmDetails = new FilmDetails(films[i]);
+  const filmsList = document.querySelectorAll(`.films-list`)[0];
 
-  const renderFilmDetails = () => {
-    render(mainContainer, filmDetails.getElement());
-    const closeDetailsButton = filmDetails
+  const filmsContainer = document.querySelectorAll(`.films-list__container`)[0];
+  const topFilmsContainer = document.querySelectorAll(
+    `.films-list__container`
+  )[1];
+  const commentedFilmsContainer = document.querySelectorAll(
+    `.films-list__container`
+  )[2];
+
+  for (let i = 0; i < NUM_FILMS; i++) {
+    const film = new FilmCard(films[i]);
+    const filmDetails = new FilmDetails(films[i]);
+
+    const renderFilmDetails = () => {
+      render(mainContainer, filmDetails.getElement());
+      const closeDetailsButton = filmDetails
+        .getElement()
+        .querySelector(`.film-details__close-btn`);
+
+      closeDetailsButton.addEventListener(`click`, () => {
+        mainContainer.removeChild(filmDetails.getElement());
+      });
+
+      document.addEventListener(`keydown`, (evt) => {
+        if (evt.keyCode === 27) {
+          mainContainer.removeChild(filmDetails.getElement());
+        }
+      });
+    };
+
+    const filmCover = film.getElement().querySelector(`.film-card__poster`);
+    const filmTitle = film.getElement().querySelector(`.film-card__title`);
+    const filmComments = film
       .getElement()
-      .querySelector(`.film-details__close-btn`);
-    closeDetailsButton.addEventListener(`click`, () => {
-      unrender(filmDetails.getElement());
-    });
-  };
+      .querySelector(`.film-card__comments`);
 
-  const filmCover = film.getElement().querySelector(`.film-card__poster`);
-  const filmTitle = film.getElement().querySelector(`.film-card__title`);
-  const filmComments = film.getElement().querySelector(`.film-card__comments`);
+    filmCover.addEventListener(`click`, renderFilmDetails);
+    filmTitle.addEventListener(`click`, renderFilmDetails);
+    filmComments.addEventListener(`click`, renderFilmDetails);
 
-  filmCover.addEventListener(`click`, renderFilmDetails);
-  filmTitle.addEventListener(`click`, renderFilmDetails);
-  filmComments.addEventListener(`click`, renderFilmDetails);
+    render(filmsContainer, film.getElement(), Position.BEFOREEND);
+  }
 
-  render(filmsContainer, film.getElement(), Position.BEFOREEND);
+  // TODO: get top rated, get most commented
+
+  // TODO: condition
+  render(filmsList, new ShowMoreButton().getElement(), Position.BEFOREEND);
+
+  render(topFilmsContainer, new FilmCard(films[0]).getElement());
+  render(topFilmsContainer, new FilmCard(films[2]).getElement());
+  render(commentedFilmsContainer, new FilmCard(films[1]).getElement());
+  // render details popup
+  // render(filmsContainer, new FilmDetails(films[2]).getElement());
 }
-
-// TODO: get top rated, get most commented
-
-// TODO: condition
-render(filmsList, new ShowMoreButton().getElement(), Position.BEFOREEND);
-
-render(topFilmsContainer, new FilmCard(films[0]).getElement());
-render(topFilmsContainer, new FilmCard(films[2]).getElement());
-render(commentedFilmsContainer, new FilmCard(films[1]).getElement());
-// render details popup
-// render(filmsContainer, new FilmDetails(films[2]).getElement());
