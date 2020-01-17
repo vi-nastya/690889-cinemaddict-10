5e1ad607ddb496a8602d5dfbfdd22d4426dc1f2b
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-17 11:24:02 -0500

    Adds AbstractSmartComponent

--- src/utils.js
+++ src/utils.js
@@ -93,3 +93,22 @@ export class AbstractComponent {
     this.getElement().classList.add(`visually-hidden`);
   }
 }
+
+export class AbstractSmartComponent extends AbstractComponent {
+  recoveryListeners() {
+    throw new Error(`Abstract method not implemented: recoveryListeners`);
+  }
+
+  rerender() {
+    const oldElement = this.getElement();
+    const parent = oldElement.parentElement;
+
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    parent.replaceChild(newElement, oldElement);
+
+    this.recoveryListeners();
+  }
+}
