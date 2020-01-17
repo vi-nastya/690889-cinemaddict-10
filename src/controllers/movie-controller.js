c91beb2daf32e29c578fa9e29808806df2e6d4fb
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-17 11:20:44 -0500

    Starts implementing MovieController

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,10 +1,20 @@
+import { FilmCard } from "../components/film-card";
+import { FilmDetails } from "../components/film-details";
+import { render } from "../utils";
+
 export class MovieController {
-  constructor(filmData, container) {
-    this._filmData = filmData;
+  constructor(container) {
     this._container = container;
+    this._filmComponent = null;
+    this._filmDetailsComponent = null;
   }
 
-  render {
-
+  render(filmData) {
+    this._filmComponent = new FilmCard(filmData);
+    this._filmDetailsComponent = new FilmDetails(filmData);
   }
+
+  _changePopupToCard() {}
+
+  _changeCardToPopup() {}
 }
