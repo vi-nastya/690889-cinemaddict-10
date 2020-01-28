542b52b45b668b61b11a73e30b6b53ada5988139
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-01-27 23:17:46 -0500

    Updates stats period selector

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -74,24 +74,7 @@ export class Statistics extends AbstractSmartComponent {
       <span class="statistic__rank-label">Sci-Fighter</span>
     </p>
 
-    <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
-      <p class="statistic__filters-description">Show stats:</p>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-      <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-      <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-      <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-      <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-      <label for="statistic-year" class="statistic__filters-label">Year</label>
-    </form>
+    ${this._getPeriodSelectorMarkup()}
 
     <ul class="statistic__text-list">
       <li class="statistic__text-item">
@@ -113,6 +96,27 @@ export class Statistics extends AbstractSmartComponent {
     </div>
 
   </section>`;
+  }
+
+  _getPeriodSelectorMarkup() {
+    return `<form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
+      <p class="statistic__filters-description">Show stats:</p>
+
+      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" ${this._currentPeriod === Period.ALL ? `checked` : ``}>
+      <label for="statistic-all-time" class="statistic__filters-label">All time</label>
+
+      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today" ${this._currentPeriod === Period.TODAY ? `checked` : ``}>
+      <label for="statistic-today" class="statistic__filters-label">Today</label>
+
+      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week" ${this._currentPeriod === Period.WEEK ? `checked` : ``}>
+      <label for="statistic-week" class="statistic__filters-label">Week</label>
+
+      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month" ${this._currentPeriod === Period.MONTH ? `checked` : ``}>
+      <label for="statistic-month" class="statistic__filters-label">Month</label>
+
+      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year" ${this._currentPeriod === Period.YEAR ? `checked` : ``}>
+      <label for="statistic-year" class="statistic__filters-label">Year</label>
+    </form>`;
   }
 
   _setPeriodChangeHandler() {
